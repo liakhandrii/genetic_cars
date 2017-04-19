d897f89becda363f75ecc0d2f9a4f9ec08fafbfc
Hlib <hlikosh@gmail.com>
2017-04-19 18:41:24 +0300

    Basic problem definition

--- analytics/analytics.py
+++ analytics/analytics.py
@@ -1,14 +1,20 @@
 
+#TODO:functions is for data acception and basic analysis
 def data_accepter():
     pass
 
+#TODO:function is for storing generations to MongoDB using pymongo
 def data_storage():
     pass
 
+#TODO:function that returns decision on keep on generatin generations or not
 def decision_making():
     pass
 
+#TODO:function that returns "stats": "high_score", "low_score", "median", "avarage", "best_id", "worse_id"
 def analytics_creation():
     pass
 
-
+#TODO:funcrion that returns JSON respons for generation machine
+def decision_analytics_merge():
+    pass
