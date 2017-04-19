dca553e5175fa1aaf21da30dc9f2a330f58a652d
Hlib <hlikosh@gmail.com>
2017-04-19 18:20:39 +0300

    Garbage cleaning before branch switch

---
+++ analytics/analytics.py
@@ -0,0 +1,14 @@
+
+def data_accepter():
+    pass
+
+def data_storage():
+    pass
+
+def decision_making():
+    pass
+
+def analytics_creation():
+    pass
+
+
--- analytics/run.py
+++ analytics/run.py
@@ -1,17 +1,15 @@
 from flask import Flask
 from flask.ext.pymongo import PyMongo
 from bson.json_util import dumps
+import os
 
-app = Flask('test')
+app = Flask('generations')
 mongo = PyMongo(app)
 
 @app.route('/connect')
 def home_page():
-	test = mongo.db.events.find({})
+	test = mongo.db.generations.find({})
 	view_this = dumps(test)
 	return str(view_this)
 
-@app.route('/visualize')
-def visualize_page():
-	pass
 
